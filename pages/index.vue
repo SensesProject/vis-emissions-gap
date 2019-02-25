1eaacfe6b328d67afb558366d91b8106ea29bfd8
Jonas <jonasparnow@gmail.com>
2019-02-25 22:13:16 +0600

    headline and subtitles added

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="wrapper">
+    <h1>The SENSES Toolkit helps you understand and communicate climate change scenarios</h1>
     <ul>
-    	<li class="learn"><n-link to="/learn">Learn</n-link></li>
-    	<li class="explore"><n-link to="/explore">Explore</n-link></li>
-    	<li class="share"><n-link to="/share">Share</n-link></li>
+    	<li class="learn"><n-link to="/learn">Learn<br /><small>to work with scenarios</small></n-link></li>
+    	<li class="explore"><n-link to="/explore">Explore<br /><small>datasets and databases</small></n-link></li>
+    	<li class="share"><n-link to="/share">Share<br /><small>knowledge and insights</small></n-link></li>
     </ul>
   </div>
 </template>
@@ -21,8 +22,16 @@
     @include wrapper-primary();
     @include grid-basic(12);
 
+    grid-template-rows: 30vh 20vh;
+
+    h1 {
+      grid-column: 3 / 12;
+      margin: 0;
+      font-size: 3rem;
+    }
+
     ul {
-    	grid-column: 4 / 10;
+    	grid-column: 3 / 12;
     	@include grid-basic(3);
 
     	li {
@@ -35,6 +44,12 @@
     			color: $color-green;
     			font-weight: 600;
     			transition: all 0.5s;
+          font-size: 2rem;
+          line-height: 1;
+
+          small {
+            font-size: 0.6em;
+          }
 
     			&:hover {
     				background-color: $color-green;
