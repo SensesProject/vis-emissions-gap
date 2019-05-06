c1d41c80af2455a184ac59f88a2c78fdad745fe4
Jonas <6gqrbj+tower923if@posteo.org>
2019-05-06 17:07:42 +0200

    Start trajectories from 2020
    #20

--- components/VisArrow.vue
+++ components/VisArrow.vue
@@ -1,14 +1,11 @@
 <template>
   <g>
     <path
-      class="arrow"
+      class="arrow arrow--area"
+      :d="area" />
+    <path
+      class="arrow arrow--path"
       :d="path" />
-    <line
-      :x1="start[0]"
-      :y1="start[1]"
-      :x2="x"
-      :y2="y"
-      class="arrow" />
     <text
       :x="center[0]"
       :y="center[1]"
@@ -37,9 +34,13 @@
       ...mapGetters([
         'currentPaths'
       ]),
-      start: function () {
+      today: function () {
         const [x, y] = last(get(find(this.currentPaths, { scenario: 'historic' }), 'values', []))
         return [this.scaleX(timeParse('%Y')(x)), this.scaleY(y)]
+      },
+      start: function () {
+        const [, y] = last(get(find(this.currentPaths, { scenario: 'historic' }), 'values', []))
+        return [this.scaleX(timeParse('%Y')(2020)), this.scaleY(y)]
       },
       x: function () {
         return this.scaleX(timeParse('%Y')(this.data[0][0]))
@@ -48,6 +49,9 @@
         return this.scaleY(this.data[0][1])
       },
       path: function () {
+        return `M${this.today.join(' ')} L ${this.start.join(' ')} L ${this.x} ${this.y}`
+      },
+      area: function () {
         const x1 = this.scaleX(timeParse('%Y')(this.data[1][0]))
         const x2 = this.scaleX(timeParse('%Y')(this.data[2][0]))
         return `M${this.start.join(' ')} L ${x1} ${this.y} L ${x2} ${this.y} Z`
